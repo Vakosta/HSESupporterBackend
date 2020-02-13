b212c8a3ee2f8084e6f6e414185d43f6377417d9
Vakosta <zzoorm@gmail.com>
2020-02-13 14:42:09 +0300

    Add message and dormitory models

--- api/admin.py
+++ api/admin.py
@@ -7,6 +7,7 @@ from api import models
 class ProblemAdmin(admin.ModelAdmin):
     list_display = (
         'title',
+        'author',
         'status',
         'created_at',
     )
@@ -17,4 +18,38 @@ class ProblemAdmin(admin.ModelAdmin):
     readonly_fields = (
         'created_at',
         'updated_at',
+    )
+
+
+@admin.register(models.Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'text',
+        'author',
+        'is_read',
+        'created_at',
+    )
+    search_fields = (
+        'text',
+        'author',
+    )
+    list_filter = (
+        'is_read',
+    )
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+
+
+@admin.register(models.Dormitory)
+class DormitoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'address',
+    )
+    search_fields = (
+        'name',
+        'address',
+        'students',
     )
--- api/models.py
+++ api/models.py
@@ -20,6 +20,7 @@ class Problem(models.Model):
                               max_length=5,
                               choices=Status.choices,
                               default=Status.OPEN)
+
     created_at = models.DateTimeField(verbose_name='дата создания',
                                       auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='дата обновление',
@@ -28,3 +29,35 @@ class Problem(models.Model):
     class Meta:
         verbose_name = 'проблема'
         verbose_name_plural = 'проблемы'
+
+
+class Message(models.Model):
+    author = models.ForeignKey(verbose_name='автор',
+                               to=User,
+                               on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='описание проблемы',
+                            max_length=10000)
+    is_read = models.BooleanField(verbose_name='прочитано',
+                                  default=False)
+
+    created_at = models.DateTimeField(verbose_name='дата создания',
+                                      auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='дата обновление',
+                                      auto_now=True)
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
+
+
+class Dormitory(models.Model):
+    name = models.CharField(verbose_name='имя общежития',
+                            max_length=200)
+    address = models.CharField(verbose_name='адрес',
+                               max_length=500)
+    students = models.ManyToManyField(User,
+                                      verbose_name='студенты')
+
+    class Meta:
+        verbose_name = 'общежитие'
+        verbose_name_plural = 'общежития'
