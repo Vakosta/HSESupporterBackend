a124f2a3c2f96e60e2ca9b013d4101cfaffa1c51
Vakosta <zzoorm@gmail.com>
2020-02-25 15:22:43 +0300

    Fix

--- HSESupporterBackend/settings.py
+++ HSESupporterBackend/settings.py
@@ -116,4 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
