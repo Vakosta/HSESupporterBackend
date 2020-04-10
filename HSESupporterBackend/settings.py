0a41eeee1615d527b7b77c716063d84bbda710f1
Vakosta <zzoorm@gmail.com>
2020-04-11 00:43:08 +0300

    Some fixes and improvements.

--- HSESupporterBackend/settings.py
+++ HSESupporterBackend/settings.py
@@ -121,6 +121,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'zzoorm@gmail.com'
+EMAIL_HOST_PASSWORD = 'KoLbasa9116782001'
+DEFAULT_FROM_EMAIL = 'confrim@hse.ru'
+DEFAULT_TO_EMAIL = 'vaannenkov@edu.hse.rur'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
--- api/views.py
+++ api/views.py
@@ -1,8 +1,28 @@
+import asyncio
+
+import aioruz
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from api import models, serializers, exceptions
+from api.serializers import UserSerializer
+
+
+async def get_student_by_email_async(email):
+    try:
+        student = await aioruz.student_info(email)
+        return student
+    except LookupError:
+        return None
+
+
+def get_student_by_email(email):
+    loop = asyncio.new_event_loop()
+    result = loop.run_until_complete(get_student_by_email_async(email))
+    return result
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -55,6 +75,7 @@ class MessagesViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         try:
+            # kek = get_student_by_email('vaafnnenkov@edu.hse.ru')
             return models.Message.objects.all().order_by('id')
         except TypeError:
             raise exceptions.Unauthorized()
@@ -65,11 +86,11 @@ class MessagesViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         try:
             problem = None
-            if 'problem' in request.data:
+            if 'problem' in request.data and request.data['problem'] != 0:
                 problem = request.data['problem']
 
             dormitory = None
-            if 'dormitory' in request.data:
+            if 'dormitory' in request.data and request.data['dormitory'] != 0:
                 dormitory = request.data['dormitory']
 
             models.Message.objects.create(
@@ -82,6 +103,22 @@ class MessagesViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_201_CREATED)
         except Exception as ex:
             return Response({"Fail": ex}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def create_auth(request):
+    serialized = UserSerializer(data=request.DATA)
+    if serialized.is_valid():
+        send_mail('Код подтверждения', 'Ваш код подтверждения.', 'confrim@hsesupporter.ru',
+                  ['vaannenkov@edu.hse.ru'], fail_silently=False)
+        User.objects.create_user(
+            serialized.init_data['email'],
+            serialized.init_data['username'],
+            serialized.init_data['password']
+        )
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DormitoriesViewSet(viewsets.ModelViewSet):
